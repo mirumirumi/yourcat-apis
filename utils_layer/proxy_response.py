32456800e19ce1be2a548b33c6da55b08ceef5f5
mirumi <info@milmemo.net>
2021-11-18 15:40:38 +0900

    [add] : CORS AllowOrigin Mapping each envs

--- utils_layer/proxy_response.py
+++ utils_layer/proxy_response.py
@@ -1,12 +1,15 @@
+import os
 import json
 from decimal import Decimal
+
+API_ALLOW_ORIGIN = os.getenv("API_ALLOW_ORIGIN")
 
 
 def _200(val=None):
     return {
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": API_ALLOW_ORIGIN
         },
         "body": json.dumps(
             val if val is not None else "no response data",
@@ -18,7 +21,7 @@ def _500(val=None):
     return {
         "statusCode": 500,
         "headers": {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Origin": API_ALLOW_ORIGIN
         },
         "body": json.dumps(
             val if val is not None else "no response data",
